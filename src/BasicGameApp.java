b380d77e6a776f199d909b553c8ab464718d7b30
lorenzopeabody28 <lorenzo_peabody28@milton.edu>
2026-05-06 09:03:55 -0400

    Initial commit

---
+++ src/BasicGameApp.java
@@ -0,0 +1,8 @@
+public class BasicGameApp {
+    public Card[] deck;
+    public Player p1;
+    public Dealer d1;
+    public boolean gameOn;
+    Player p1 = new Player();
+    Dealer d1 = new Dealer();
+}
---
+++ src/Player.java
@@ -0,0 +1,7 @@
+public class Player {
+    public int cardTotal;
+    public boolean isBust;
+    public Card[] hand;
+    public boolean isHit;
+    public String name;
+}
