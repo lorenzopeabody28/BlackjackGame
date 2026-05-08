0298932bd6ac629eb4950bcc36f39637b598cc46
lorenzopeabody28 <lorenzo_peabody28@milton.edu>
2026-05-08 14:56:14 -0400

    Inclass 5/8

--- src/BasicGameApp.java
+++ src/BasicGameApp.java
@@ -1,8 +1,28 @@
+import java.util.Base64;
+
 public class BasicGameApp {
     public Card[] deck;
     public Player p1;
     public Dealer d1;
     public boolean gameOn;
-    Player p1 = new Player();
-    Dealer d1 = new Dealer();
+    //Player p1 = new Player();
+    //Dealer d1 = new Dealer();
+
+    //todo: Print deck method, finish if statements in Card class (eliminate nulls)
+
+    public static void main(String[] args) {
+        BasicGameApp a = new BasicGameApp();
+    }
+    public BasicGameApp(){
+        System.out.println("Welcome to Blackjack!");
+        deck = new Card[52];
+
+        for(int x = 0; x < 13; x++){
+            deck[x] = new Card(10,x, "Hearts");
+            deck[x].printInfo();
+        }
+
+
+
+    }
 }
--- src/Card.java
+++ src/Card.java
@@ -3,4 +3,19 @@ public class Card {
     public String name;
     public String suit;
 
+    public Card(int pValue, int pName, String pSuit) {
+        value = pValue;
+        if(pName == 0){
+            name = "Ace";
+            value = 11;
+        } else if(pName ==1){
+            name = "two";
+            value = 2;
+        }
+        suit = pSuit;
+    }
+        public void printInfo(){
+            System.out.println("The " + name + "of " + suit + "is worth " + value + "points!");
+
+    }
 }
